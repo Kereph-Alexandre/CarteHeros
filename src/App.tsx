6ddd9d1080ca03d1cf118a7cb3bf185cf76f00db
Alexandre Bouriez <alexandre.bouriez@laposte.net>
2023-01-17 09:27:11 +0100

    feat: Ajout de la bannière Header :sparkles:

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import "./App.css";
 
 import React from "react";
 import Router from "./routes/routes";
+import { Header } from "./composants/communs/header/header";
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <Header />
       <Router />
     </div>
   );
